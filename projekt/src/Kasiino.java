926271e68a79252a1984a54254832db2e47b2b1b
Markkus <markkuspokrovski@gmail.com>
2025-03-17 14:28:52 +0200

    valmis main

--- projekt/src/Kasiino.java
+++ projekt/src/Kasiino.java
@@ -13,5 +13,33 @@ public class Kasiino {
 
     public void alusta(){
         System.out.println("Tere tulemast kasiinosse, " + mängija.getNimi() + "!");
+
+        boolean töötab = true;
+        while (töötab){
+            System.out.println("Mida soovid mängida=");
+            System.out.println("1 - Blackjack");
+            System.out.println("2 - Rulett");
+            System.out.println("3 - lahku kasiinost");
+            System.out.println("Valik: ");
+
+            int choice = scanner.nextInt();
+
+            if (choice == 1) {
+                System.out.println("Blackjack pole veel valmis!"); // Hiljem lisame mängu
+            } else if (choice == 2) {
+                System.out.println("Rulett pole veel valmis!"); // Hiljem lisame mängu
+            } else if (choice == 3) {
+                System.out.println("Aitäh mängimast! Lahkud kasiinost.");
+                töötab = false;
+            } else {
+                System.out.println("Vale valik! Proovi uuesti.");
+            }
+        }
+        scanner.close();
+    }
+
+    public static void main(String[] args) {
+        Kasiino kasiino = new Kasiino();
+        kasiino.alusta();
     }
 }
