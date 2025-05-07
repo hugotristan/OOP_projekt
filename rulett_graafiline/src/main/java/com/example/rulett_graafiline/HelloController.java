33c3dbe4f597a25456b47ea6f7c2cedbf3e40b1f
hugotristan <hugo.tristan.tammik@ut.ee>
2025-05-07 16:19:16 +0300

    v2 peaks muutuma vastavalt akna suurusele

--- rulett_graafiline/src/main/java/com/example/rulett_graafiline/HelloController.java
+++ rulett_graafiline/src/main/java/com/example/rulett_graafiline/HelloController.java
@@ -1,7 +1,17 @@
 package com.example.rulett_graafiline;
 
+import javafx.animation.RotateTransition;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextInputDialog;
+import javafx.util.Duration;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class HelloController {
     @FXML
@@ -12,3 +22,4 @@ public class HelloController {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
+
